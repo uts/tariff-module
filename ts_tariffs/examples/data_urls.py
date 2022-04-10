f2cf434acc603e93485cab57903184b8161efe8c
FishAndSauce <joseph.wyndham@gmail.com>
2022-04-10 13:54:42 +1000

    new version number

--- ts_tariffs/examples/data_urls.py
+++ ts_tariffs/examples/data_urls.py
@@ -1,3 +1,3 @@
 
-
+# Source: Kaggle: https://www.kaggle.com/datasets/uciml/electric-power-consumption-data-set?resource=download
 minute_household_consumption = 'https://tariff-model-test-data.s3.ap-southeast-2.amazonaws.com/30_minute_household_consumption.csv'
